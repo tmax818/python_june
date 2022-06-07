b881a7320bd640f57bb6ee65e7eb484e4902353c
tmax818 <tylermaxwell661@gmail.com>
2022-06-07 10:02:01 -0700

    algo

--- algos/day02/main.py
+++ algos/day02/main.py
@@ -1,25 +1,81 @@
 # Print 1-255
 # Print all the integers from 1 to 255.
 
+# for i in range(1,256):
+#     print(i)
 
 # Print Sum 0-255
 # Print integers from 0 to 255, and with each integer print the sum so far.
 
+# sum = 0
+# for i in range(256):
+#     sum = sum + i
+#     print("the number is",i)
+#     print("The sum is currently", sum)
 
 # Find and Print Max
 # Given an array, find and print its largest element.
+test_array = [1,2,3,4, 654,5,6,7,455]
+test_array1 = [1,2,3,4]
 
+# def printMax(theArrayParam):
+#     max = theArrayParam[0]
+#     for i in theArrayParam:
+#         if(i > max):
+#             max = i
+#     print(max)
+
+
+# printMax(test_array)
+# printMax(test_array1)
 
 # Array with Odds
 # Create an array with all the odd integers between 1 and 255 (inclusive).
 
+# oddList = []
+
+# for i in range(1,256):
+#     if(i % 2 != 0):
+#         oddList.append(i)
+# print(oddList)
 
 # Greater Than Y
 # Given an array and a value Y, count and print the number of array values greater than Y.
 
+# test_array = [1,2,3,4, 654,5,6,7,455]
+# test_array1 = [1,2,3,4]
+
+# def greaterThanY(arrParam, yParam):
+#     count = 0
+#     for i in arrParam:
+#         if(i > yParam):
+#             count += 1
+#     print(count)
+
+
+
+# greaterThanY(test_array1, 20)
+
 
 # Max, Min, Average
 # Given an array, print the max, min and average values for that array.
+
+testarray = [1,2,3, 5, 78, 4]
+
+def maxMinAvg(arrParam):
+    max = arrParam[0]
+    min = arrParam[0]
+    sum = 0
+    for i in arrParam:
+        sum += i
+        if(i > max):
+            max = i
+        if(i < min):
+            min = i
+    print(max, min, sum/len(arrParam))
+
+maxMinAvg(testarray)
+
 
 
 # Swap String For Array Negative Values

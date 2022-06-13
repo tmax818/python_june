77c0aaf1bceb7e47b0b66b7a3e5c893e72501ee7
tmax818 <tylermaxwell661@gmail.com>
2022-06-13 15:24:13 -0700

    update

--- assignments/practice/functions_basic_ii/test.py
+++ assignments/practice/functions_basic_ii/test.py
@@ -6,11 +6,21 @@ print(type(file.countdown))
 
 class TestFunctions(unittest.TestCase):
 
-    def test_countdown_exists(self):
-        self.assertIsInstance(file.countdown, function)
-
     def test_countdown(self):
         self.assertEqual(file.countdown(5), [5,4,3,2,1,0])
+
+    def test_print_and_return(self):
+        pass
+
+
+    def test_first_plus_length(self):
+        pass
+
+    def test_values_greater_than_second(self):
+        pass
+
+    def test_length_and_value(self):
+        pass
 
     
 

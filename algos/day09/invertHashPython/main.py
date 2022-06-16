84beb14e9ca16de872a1e20a532351fc0b9c45e2
tmax818 <tylermaxwell661@gmail.com>
2022-06-16 08:47:10 -0700

    update

---
+++ algos/day09/invertHashPython/main.py
@@ -0,0 +1,9 @@
+
+#   Invert Hash
+#   A hash table / hash map is an obj / dictionary
+#   Given an object / dict,
+#   return a new object / dict that has the keys and the values swapped so that the keys become the values and the values become the keys
+
+
+dict1 = { 'name': 'Zaphod', 'charm': 'high', 'morals': 'dicey' }
+expected1 = { 'Zaphod': 'name', 'high': 'charm', 'dicey': 'morals'}

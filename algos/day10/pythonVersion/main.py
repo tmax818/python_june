310854deceebebe4caa1ce309c8cb0283c5ff9a8
tmax818 <tylermaxwell661@gmail.com>
2022-06-17 08:35:22 -0700

    update

---
+++ algos/day10/pythonVersion/main.py
@@ -0,0 +1,29 @@
+# /* 
+#   String: Is Palindrome
+#   Create a function that returns a boolean whether the string is a strict palindrome. 
+#     - palindrome = string that is same forwards and backwards
+  
+#   Do not ignore spaces, punctuation and capitalization
+#  */
+
+str1 = "a x a"
+expected1 = True
+  
+str2 = "racecar"
+expected2 = True
+  
+str3 = "Dud"
+expected3 = False
+  
+str4 = "oho!"
+expected4 = False
+  
+#   /**
+#    * Determines if the given str is a palindrome (same forwards and backwards).
+#    * - Time: O(?).
+#    * - Space: O(?).
+#    * @param {string} str
+#    * @returns {boolean} Whether the given str is a palindrome or not.
+#    */
+def isPalindrome(str):
+    pass
